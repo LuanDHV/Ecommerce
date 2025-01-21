3559b05d10d9c15791e8b88d7f8ef351cddf4fde
LuanDHV <luandhv1406@gmail.com>
2025-01-21 22:50:01 +0700

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { Footer } from "antd/es/layout/layout";
 import Header from "./components/Header";
 
 export default function Home() {
@@ -5,6 +6,7 @@ export default function Home() {
     <>
       <Header />
       <div className="text-5xl font-bold text-red-500">Home Page</div>
+      <Footer />
     </>
   );
 }
